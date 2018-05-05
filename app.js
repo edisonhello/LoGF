651ffd0ca90321894240081a9a14b8efd00eb874
MiccWan <michaelwan0906@hotmail.com>
2018-05-05 12:13:41 +0800

    construction

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
 'use strict'
 
+var express = require('express');
+var app     = express();
+var config  = require('./config.json');
+var port    = config.port;
 
+app.use('/', express.static(__dirname + '/assest'))
+
+app.get('/', function(req,res) {
+    res.sendFile(__dirname + '/index.html', function() {
+        res.end();
+    });
+});
+
+app.listen(port, function () {
+    console.log("Server listening on port", port);
+});
