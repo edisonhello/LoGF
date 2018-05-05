587eb3d0bd45f9c7898bfd79c5df709a7ed678bc
edisonhello <edisonhello@hotmail.com>
2018-05-05 18:13:59 +0800

    Generated game object, handle some requests

--- assets/js/player.js
+++ assets/js/player.js
@@ -1,13 +1,14 @@
 'use strict'
 
 module.exports = {
-    Player: function(name) {
+    Player: function(name, initLocation) {
 
         // player name
         this.playerName = name;
         
         // player status
         this.hp          = 20;
+        this.maxhp       = 20;
         this.action      = 10;
         this.saturation  = 10;
         this.oxygen      = 10;
@@ -15,6 +16,7 @@ module.exports = {
         this.attack      = 2;
         this.defence     = 0;
         this.potential   = 0;
+        this.location    = initLocation;
 
         // resource
         this.wood        = 0;
@@ -25,6 +27,5 @@ module.exports = {
         // play harvest action allowed
         this.canHarvestStone = false
         this.canHarvestDiamond = false
-
     }
 };
