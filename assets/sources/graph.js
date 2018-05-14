b4efe1617c12184bf08343eed847afa69cd6fde0
ChexterWang <31370607+ChexterWang@users.noreply.github.com>
2018-05-14 11:41:35 +0800

    update graphjs for players

--- assets/sources/graph.js
+++ assets/sources/graph.js
@@ -20,9 +20,10 @@ var options   = {
 }
 
 // draw graph
-function draw(graphNodes, graphEdges) {
-    graphData.nodes = new vis.DataSet(graphNodes);
-    graphData.edges = new vis.DataSet(graphEdges);
+function draw(graphNodes, graphEdges, graphPlayers) {
+    graphData.nodes   = new vis.DataSet(graphNodes);
+    graphData.edges   = new vis.DataSet(graphEdges);
+    graphData.players = new vis.DataSet(graphPlayers);
     network = new vis.Network(graph, graphData, options);
 
     network.on('deselectNode', function (params) {
