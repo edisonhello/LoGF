cb9835d84b2b7e757c25b0ff6aa005e80f81fcc1
ChexterWang <31370607+ChexterWang@users.noreply.github.com>
2018-05-15 14:50:18 +0800

    node hovering enabled

--- assets/sources/graph.js
+++ assets/sources/graph.js
@@ -1,21 +1,28 @@
 // setup netword data
 var graph     = document.getElementById('graph');
 var graphData = {}, network;
-var options   = {
-    'height'      : 'calc(100% - 2px)',
-    'width'       : 'calc(100% - 1px)',
-    'interaction' : { 'dragNodes':false },
-    'edges'       : { 'width':2 },
-    'physics'     : { 'enabled':false },
-    'nodes'       : {
-        'chosen': { 'node':nodesOnClick },
+
+ //Node Pattern
+var options  = {
+    'height'       : 'calc(100% - 2px)',
+    'width'        : 'calc(100% - 1px)',
+    'interaction'  : {
+        'dragNodes': false,
+        'hover'    : true
+    },
+    'edges'        : { 'width':2 },
+    'physics'      : { 'enabled':false },
+    'nodes'        : {
+        'chosen'     : { 'node':nodesOnClick },
         'borderWidth': 2,
-        'shape': 'dot',
-        'size' : 30,
-        'font' : {
-            'size' : 32,
-            'color': '#ffffff'
-        }
+        'shape'      : 'circularImage',
+        'size'       : 30,
+        'font'       : {
+              'size' : 32,
+              'color': '#ffffff',
+            'vadjust': +5
+        },
+        'level'      : 1
     }
 }
 
@@ -27,11 +34,11 @@ function draw(graphNodes, graphEdges) {
 
     network.on('deselectNode', function (params) {
         nodeInfo.innerHTML = '';
-    })
+    });
 
     document.addEventListener('keydown', function(event){
         if(event.keyCode === 27){
             unselectNodes();
         }
-    })
+    });
 }
